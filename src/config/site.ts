1cb97dd839880da79c45db684017de8c2fdac7ac
eKoh951 <greatekoh@gmail.com>
2024-09-16 21:36:35 -0600

    improved copy a 2nd time

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Escuela Libre de Desarrollo de Software",
-  description: "Escuela de programación libre y gratuita. Aprende a programar sin costo, con contenido mantenido por la comunidad.",
+  description: "Aprende a programar sin costo, con contenido mantenido por la comunidad.",
   url: "https://escuelalibre.dev",
   githubUrl: "https://github.com/eKoh951/escuelalibre.dev",
   links: {
